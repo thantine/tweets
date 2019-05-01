5588746b1121604fbcbf5d14fdc0499fa17a91a2
vinh <vinhbq1902@gmail.com>
2019-05-01 23:22:34 +0700

    initial express and mongodb

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,8 +1,18 @@
-import { createStore } from "redux";
-import { defaultState } from "../../server/defaultState";
+import { createStore, applyMiddleware } from "redux";
+import { createEpicMiddleware } from 'redux-observable';
+
+const epicMiddleware = createEpicMiddleware();
+
+const initialState = {
+  messages: []
+}
 
 export const store = createStore(
-  function reducers(state = defaultState, action) {
-    return state;
-  }
+  function reducers(state = initialState, action) {
+    switch(action.type) {
+      default:
+        return state;
+    }
+  },
+  applyMiddleware(epicMiddleware)
 );
